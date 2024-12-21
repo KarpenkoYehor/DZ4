9972125e00ca656a77bc267f02f42d58cf310f08
Egor Karpenko <92591674+Egor-Karpenko@users.noreply.github.com>
2024-12-21 19:51:14 +0200

    fix: pass_length

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)  # Додаємо поле для пароля
+    password = db.Column(db.String(255), nullable=False)  # Додаємо поле для пароля
 
     def __repr__(self):
         return f'<User {self.name}>'
